c4333485b55bc578b1f463c851d944ed9259dc29
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-02-26 15:00:51 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Stock from "./pages/Stock";
+import About from "./pages/About";
+import Nav from "./components/Nav";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stocks/:symbol" element={<Stock />} />
+        <Route path="/stocks" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <h1>
+      Stocks App using REACT to display active stocks under dashboard or click
+      on each company name to get more info
+    </h1>
+  );
+}
